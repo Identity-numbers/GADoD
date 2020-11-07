2b1432265b5271d5314baa758d9319b573e2d7aa
Linus Jarbo <linus@linusjarbo.com>
2020-11-07 21:49:34 +0100

    Intial commit
    First commit

---
+++ GADoD/Assets/Scripts/Helper/RandomClass.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomClass : MonoBehaviour
+{
+    //return val on normal dist from 0-1
+
+    //return even random 0-1
+
+    //return pyramid random 0-1
+
+
+}
---
+++ GADoD/Assets/Scripts/Main/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    /*
+    //TODO:
+
+    -Init population
+        -seed population
+    -create random class, return normal dist of values
+
+    -create test environment
+
+
+
+
+
+    */
+}
